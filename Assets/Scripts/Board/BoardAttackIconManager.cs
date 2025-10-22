b95397a8b3e5250f3588469a7ea8588b6753b870
TimVoxel <tim.buyalski@gmail.com>
2025-10-22 15:25:16 +0300

    Moved the editor code into the editor folder to allow building

--- Assets/Scripts/Board/BoardAttackIconManager.cs
+++ Assets/Scripts/Board/BoardAttackIconManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using TMPro;
-using TMPro.EditorUtilities;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
